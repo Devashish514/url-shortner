ba51286a8721d5cfc6ad57cb7c3e5d13ace2a443
Devashish514 <devagosw193@gmail.com>
2022-12-26 20:45:26 +0530

    try-hosting

--- src/database/postgres.index.js
+++ src/database/postgres.index.js
@@ -1,6 +1,9 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DATABASE_URL,{dialect:"postgres"});
+const sequelize = new Sequelize({
+    url: process.env.DATABASE_URL, 
+    dialect: "postgres"
+});
 
 sequelize.sync();
 
